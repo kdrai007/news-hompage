0f3ab5726499c3e5d6bdbbfdda616d2988ff4f99
kdrai007 <987kundanrai@gmail.com>
2023-04-19 09:55:45 +0530

    fixing everything

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function () {
+  $(".menu-btn").click(function () {
+    const img = $(".menu-btn img");
+    $(".menu-show").slideToggle(100);
+    document.body.classList.toggle("body-color");
+    if (img.attr("src") === "./assets/images/icon-menu.svg") {
+      img.attr("src", "./assets/images/icon-menu-close.svg");
+    } else {
+      img.attr("src", "./assets/images/icon-menu.svg");
+    }
+  });
+});
